c0f514c124463987ddbc379df4c9a4f2da923074
terefet <tutg24@gmail.com>
2021-12-24 18:24:54 +0300

    w3school react tutorial commit

---
+++ src/Car.js
@@ -0,0 +1,12 @@
+
+
+function Car(props) {
+    return (<>
+    <li>Hi, I am a {props.brand} Car!</li>
+   
+    </>)
+  }
+
+  
+  
+  export default Car;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Car from './Car.js';
+import Garage from './Car.js';
+import App from './App.js';
 
-function Lake({ name }) {
-  return <h1>{name}</h1>;
-}
 
-function App() {
-  return (
-    <div>
-      <Lake name="Lake Tahoe" />
-      <Lake name="Angora Lake" />
-      <Lake name="Shirley Lake" />
-    </div>
-  );
-}
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+ReactDOM.render(<App/> ,document.getElementById("root")
+
 );
 
